4ee13904570c45e796f24caf0c50c11401241cea
Sudip Lama <slama@us.ibm.com>
2016-07-04 10:12:35 -0400

    Initial Product chain code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,24 +1,14 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 package main
 
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"encoding/json"
+	"time"
+	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -26,6 +16,24 @@ import (
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
+
+var productIndexStr = "_productindex"				//name for the key/value that will store a list of all products
+
+type Product struct{
+	Product_Id string `json:"product_id"`
+	Category string `json:"category"`
+	Product_Description string `json:"product_description"`
+	Availability_Start_Date string `json:"availability_start_date"`
+	Availability_End_Date string `json:"availability_end_date"`
+	List_Price int `json:"list_price"`
+	Currency string `json:"currency"`
+	Price_Start_Date string `json:"price_start_date"`
+	Price_End_Date string `json:"price_end_date"`
+	User_Type string `json:"user_type"`
+}
+
+
+
 
 // ============================================================================================================================
 // Main
@@ -37,38 +45,283 @@ func main() {
 	}
 }
 
-// Init resets all the things
+// ============================================================================================================================
+// Init - reset all the things
+// ============================================================================================================================
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	var Aval int
+	var err error
+
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
+	// Initialize the chaincode
+	Aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+
+	// Write the state to the ledger
+	err = stub.PutState("abc", []byte(strconv.Itoa(Aval)))				//making a test var "abc", I find it handy to read/write to it right away to test the network
+	if err != nil {
+		return nil, err
+	}
+
+	var empty []string
+	jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the index
+	err = stub.PutState(productIndexStr, jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+
 	return nil, nil
 }
+// ============================================================================================================================
+// Run - Our entry point for Invocations - [LEGACY] obc-peer 4/25/2016
+// ============================================================================================================================
+func (t *SimpleChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	fmt.Println("run is running " + function)
+	return t.Invoke(stub, function, args)
+}
 
-// Invoke is our entry point to invoke a chaincode function
+// ============================================================================================================================
+// Run - Our entry point for Invokcations
+// ============================================================================================================================
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
+	fmt.Println("run is running " + function)
 
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "delete" {										//deletes an entity from its state
+		res, err := t.Delete(stub, args)
+		return res, err
+	} else if function == "write" {											//writes a value to the chaincode state
+		return t.Write(stub, args)
+	} else if function == "init_product" {									//create a new product
+		return t.init_product(stub, args)
+	} else if function == "set_user_type" {										//change user_type of a product
+		res, err := t.set_user_type(stub, args)
+		return res, err
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
+	fmt.Println("run did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function invocation")
 }
 
-// Query is our entry point for queries
+// ============================================================================================================================
+// Query - Our entry point for Queries
+// ============================================================================================================================
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" {													//read a variable
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query")
+}
+
+// ============================================================================================================================
+// Read - read a variable from chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+	}
+
+	name = args[0]
+	valAsbytes, err := stub.GetState(name)									//get the var from chaincode state
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil													//send it onward
+}
+
+// ============================================================================================================================
+// Delete - remove a key/value pair from state
+// ============================================================================================================================
+func (t *SimpleChaincode) Delete(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	name := args[0]
+	err := stub.DelState(name)													//remove the key from chaincode state
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
+	//get the product index
+	productsAsBytes, err := stub.GetState(productIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get product index")
+	}
+	var productIndex []string
+	json.Unmarshal(productsAsBytes, &productIndex)								//un stringify it aka JSON.parse()
+
+	//remove product from index
+	for i,val := range productIndex{
+		fmt.Println(strconv.Itoa(i) + " - looking at " + val + " for " + name)
+		if val == name{															//find the correct marble
+			fmt.Println("found marble")
+			productIndex = append(productIndex[:i], productIndex[i+1:]...)			//remove it
+			for x:= range productIndex{											//debug prints...
+				fmt.Println(string(x) + " - " + productIndex[x])
+			}
+			break
+		}
+	}
+	jsonAsBytes, _ := json.Marshal(productIndex)									//save new index
+	err = stub.PutState(productIndexStr, jsonAsBytes)
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Write - write variable into chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) Write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var name, value string // Entities
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
+	}
+
+	name = args[0]															//rename for funsies
+	value = args[1]
+	err = stub.PutState(name, []byte(value))								//write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Init Marble - create a new marble, store into chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) init_product(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var err error
+
+	//   0       1       2     3
+	// "asdf", "blue", "35", "bob"
+	if len(args) != 10 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	}
+
+	fmt.Println("- start init marble")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return nil, errors.New("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
+	if len(args[4]) <= 0 {
+		return nil, errors.New("5th argument must be a non-empty string")
+	}
+	if len(args[5]) <= 0 {
+		return nil, errors.New("6th argument must be a non-empty string")
+	}
+	if len(args[6]) <= 0 {
+		return nil, errors.New("7th argument must be a non-empty string")
+	}
+	if len(args[7]) <= 0 {
+		return nil, errors.New("8th argument must be a non-empty string")
+	}
+	if len(args[8]) <= 0 {
+ 	 return nil, errors.New("9th argument must be a non-empty string")
+  }
+	list_price, err := strconv.Atoi(args[5])
+	if err != nil {
+		return nil, errors.New("5rd argument must be a numeric string")
+	}
+
+
+
+	user_type := strings.ToLower(args[9])
+
+	str := `{"product_id": "` + args[0] + `", "category": "` + args[1] +
+	 `", "product_description": ` + args[2] + `, "availability_start_date": "` + args[3] +
+	 `, "availability_end_date": ` + args[4] + `, "list_price": "` + strconv.Itoa(list_price) +
+	 `, "currency": ` + args[6] + `, "price_start_date": "` + args[7] +
+	 `, "price_end_date": ` + args[8]+ `, "user_type": "` + user_type +
+	  `"}`
+	err = stub.PutState(args[0], []byte(str))								//store marble with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	//get the product index
+	productsAsBytes, err := stub.GetState(productIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get product index")
+	}
+	var productIndex []string
+	json.Unmarshal(productsAsBytes, &productIndex)							//un stringify it aka JSON.parse()
+
+	//append
+	productIndex = append(productIndex, args[0])								//add product name to index list
+	fmt.Println("! product index: ", productIndex)
+	jsonAsBytes, _ := json.Marshal(productIndex)
+	err = stub.PutState(productIndexStr, jsonAsBytes)						//store name of product
+
+	fmt.Println("- end init product")
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Set User type Permission on Product
+// ============================================================================================================================
+func (t *SimpleChaincode) set_user_type(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	var err error
+
+	//   0       1
+	// "name", "bob"
+	if len(args) < 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	fmt.Println("- start set user type")
+	fmt.Println(args[0] + " - " + args[1])
+	productAsBytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New("Failed to get thing")
+	}
+	res := Product{}
+	json.Unmarshal(productAsBytes, &res)										//un stringify it aka JSON.parse()
+	res.User_Type = args[1]														//change the user type
+
+	jsonAsBytes, _ := json.Marshal(res)
+	err = stub.PutState(args[0], jsonAsBytes)								//rewrite the Product with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("- end set user type")
+	return nil, nil
+}
+
+
+
+// ============================================================================================================================
+// Make Timestamp - create a timestamp in ms
+// ============================================================================================================================
+func makeTimestamp() int64 {
+    return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
 }
